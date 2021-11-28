9da33c1923ca1d0400267386ab3e5450fe566575
uhobeike <tatuhiro315@ezweb.ne.jp>
2021-11-28 17:33:31 +0900

    Fix

--- scripts/map2gazebo_docker_run.sh
+++ scripts/map2gazebo_docker_run.sh
@@ -4,14 +4,13 @@ docker pull ubeike/ros-melodic-map2gazebo
 
 xhost +local:docker
 docker run -it --rm \
-    -v $1:/home/catkin_ws/src/map2gazebo/models:rw \
-    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-    -e DISPLAY=$DISPLAY \
-    -e QT_X11_NO_MITSHM=1 \
+    -v $1/map:/home/catkin_ws/src/map2gazebo/map:rw \
+    -v $1/config:/home/catkin_ws/src/map2gazebo/config:rw \
+    -v $1/outputs/models:/home/catkin_ws/src/map2gazebo/models:rw \
     -e LIBGL_ALWAYS_INDIRECT=1 \
     --privileged \
     --net=host \
-    --name raspicat_gmapping \
+    --name raspicat_map2gazebo \
 ubeike/ros-melodic-map2gazebo /bin/bash -i -c \
       "bash /run_map2gazebo.sh $2"
 xhost -local:docker
